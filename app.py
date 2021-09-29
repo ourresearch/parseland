160e33fad8c5dea2aa74c61add9bfbfe7e5eb3ae
Casey M <caseym@gmail.com>
2021-09-29 13:07:04 -0500

    enable requests cache

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
+import requests_cache
 
 app = Flask(__name__)
 CORS(app)
+
+requests_cache.install_cache(cache_name='api_cache', backend='sqlite', expire_after=180)
 
 app.config["JSON_SORT_KEYS"] = False
