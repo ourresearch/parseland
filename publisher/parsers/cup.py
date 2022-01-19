63514fc7c93b8ef87440dd274ca31ca709d93dff
Richard Orr <richard@ourresearch.org>
2022-01-19 14:32:24 -0500

    parse CUP abstracts

--- publisher/parsers/cup.py
+++ publisher/parsers/cup.py
@@ -12,7 +12,7 @@ class CUP(PublisherParser):
         return self.soup.find("div", class_="author")
 
     def parse(self):
-        results = []
+        result_authors = []
         authors = self.soup.findAll("div", class_="author")
         for author in authors:
             name = author.find("dt").text
@@ -24,37 +24,43 @@ class CUP(PublisherParser):
                 for organization in affiliation_soup.stripped_strings:
                     affiliations.append(organization.strip())
 
-            results.append(AuthorAffiliations(name=name, affiliations=affiliations))
-        return results
+            result_authors.append(AuthorAffiliations(name=name, affiliations=affiliations))
+        return {
+            "authors": result_authors,
+            "abstract": self.parse_abstract_meta_tags()
+        }
 
     test_cases = [
         {
             "doi": "10.1017/S1355770X21000218",
-            "result": [
-                {
-                    "name": "Julien Wolfersberger",
-                    "affiliations": [
-                        "Université Paris-Saclay, INRAE, AgroParisTech, Economie Publique, Thiverval-Grignon, France",
-                        "Climate Economics Chair, Palais Brongniart, Paris, France",
-                    ],
-                },
-                {
-                    "name": "Gregory S. Amacher",
-                    "affiliations": [
-                        "Virginia Polytechnic Institute and State University, Blacksburg, VA, USA"
-                    ],
-                },
-                {
-                    "name": "Philippe Delacote",
-                    "affiliations": [
-                        "Climate Economics Chair, Palais Brongniart, Paris, France",
-                        "BETA, Université Lorraine, INRAE, AgroParisTech, Nancy, France",
-                    ],
-                },
-                {
-                    "name": "Arnaud Dragicevic",
-                    "affiliations": ["IRSTEA, Clermont-Ferrand, Aubiere, France"],
-                },
-            ],
+            "result": {
+                "authors": [
+                    {
+                        "name": "Julien Wolfersberger",
+                        "affiliations": [
+                            "Université Paris-Saclay, INRAE, AgroParisTech, Economie Publique, Thiverval-Grignon, France",
+                            "Climate Economics Chair, Palais Brongniart, Paris, France",
+                        ],
+                    },
+                    {
+                        "name": "Gregory S. Amacher",
+                        "affiliations": [
+                            "Virginia Polytechnic Institute and State University, Blacksburg, VA, USA"
+                        ],
+                    },
+                    {
+                        "name": "Philippe Delacote",
+                        "affiliations": [
+                            "Climate Economics Chair, Palais Brongniart, Paris, France",
+                            "BETA, Université Lorraine, INRAE, AgroParisTech, Nancy, France",
+                        ],
+                    },
+                    {
+                        "name": "Arnaud Dragicevic",
+                        "affiliations": ["IRSTEA, Clermont-Ferrand, Aubiere, France"],
+                    },
+                ],
+                "abstract": "We develop a model of optimal land allocation in a developing economy that features three possible land uses: agriculture, primary and secondary forests. The distinction between those forest types reflects their different contributions in terms of public goods. In our model, reforestation is costly because it undermines land title security. Using the forest transition concept, we study long-term land-use change and explain important features of cumulative deforestation across countries. Our results shed light on the speed at which net deforestation ends, on the effect of tenure costs in this process, and on composition in steady state. We also present a policy analysis that emphasizes the critical role of institutional reforms addressing the costs of both deforestation and tenure in order to promote a transition. We find that focusing only on net forest losses can be misleading since late transitions may yield, upon given conditions, a higher level of environmental benefits."
+            }
         },
     ]
