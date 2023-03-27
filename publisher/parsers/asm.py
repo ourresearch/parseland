ff8aedf749f8a5f5d3c6da85a5593d8e63a57229
nolan <nolanm1995@live.com>
2023-03-27 10:46:18 -0500

    Improve author/affiliation parsing (ASM)

--- publisher/parsers/asm.py
+++ publisher/parsers/asm.py
@@ -8,19 +8,20 @@ class ASM(PublisherParser):
         return self.domain_in_meta_og_url('journals.asm.org')
 
     def authors_found(self):
-        return bool(self.soup.select("header[property=author]"))
+        return bool(self.soup.select("[property=author]"))
 
     def parse_authors(self):
-        authors = []
-        author_tags = self.soup.select("header[property=author]")
+        authors = {}
+        author_tags = self.soup.select("[property=author]")
         for author_tag in author_tags:
             given_name = author_tag.select_one('[property=givenName]').text
             family_name = author_tag.select_one('[property=familyName]').text
             name = f'{given_name} {family_name}'
-            affs = [tag.text for tag in author_tag.select('[property=organization]')]
+            affs = [tag.text for tag in (author_tag.select('[property=organization]') or author_tag.select('[property=affiliation]'))]
             author = {'name': name, 'affiliations': affs, 'is_corresponding': bool(author_tag.select('a[href*=email-protection]'))}
-            authors.append(author)
-        return authors
+            if name not in authors or (not authors[name]['affiliations'] and author['affiliations']):
+                authors[name] = author
+        return list(authors.values())
 
     def parse_abstract(self):
         return self.soup.select_one('section#abstract [role=paragraph]').text
