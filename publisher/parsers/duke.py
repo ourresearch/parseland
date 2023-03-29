4206a75c06ce8cd9ac20db86215559692905888a
nolan <nolanm1995@live.com>
2023-03-29 09:31:09 -0500

    Parse meta tags in Duke parser

--- publisher/parsers/duke.py
+++ publisher/parsers/duke.py
@@ -2,14 +2,10 @@ from publisher.parsers.parser import PublisherParser
 
 
 class Duke(PublisherParser):
-
-    def authors_found(self):
-        pass
+    parser_name = 'duke'
 
     def parse(self):
-        pass
-
-    parser_name = 'duke'
+        return self.parse_meta_tags()
 
     def is_publisher_specific_parser(self):
         return self.domain_in_meta_og_url('dukeupress.edu')
