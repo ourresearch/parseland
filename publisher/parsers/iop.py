127136346b7052f146a44764c0ea9abcb7667413
nolan <nolanm1995@live.com>
2023-04-05 13:51:02 -0500

    Add IOP parser (previous version only parsed meta tags)

--- publisher/parsers/iop.py
+++ publisher/parsers/iop.py
@@ -1,5 +1,10 @@
+import re
+
 from exceptions import UnusualTrafficError
 from publisher.parsers.parser import PublisherParser
+import unicodedata
+
+from publisher.parsers.utils import strip_prefix
 
 
 class IOP(PublisherParser):
@@ -20,8 +25,18 @@ class IOP(PublisherParser):
         return self.soup.find("meta", {"name": "citation_author"})
 
     def parse(self):
+        authors = self.parse_author_meta_tags()
+        author_email_tags = self.soup.select('div[class*=art-email-addresses] a[href*=mailto]')
+        for email_tag in author_email_tags:
+            email = strip_prefix('mailto:', email_tag['href'])
+            for author in authors:
+                name = author['name'].lower()
+                normalized_name = unicodedata.normalize('NFD', name).encode('ascii', 'ignore').decode()
+                split = [part for part in re.split('[ ,]', normalized_name.strip()) if len(part) > 1]
+                if any([part in email.split('@')[0] for part in split]):
+                    author['is_corresponding'] = True
         # displayed author affiliations are not available in the content, so we have to use meta tags.
-        return self.parse_author_meta_tags()
+        return {'authors': authors, 'abstract': self.parse_abstract_meta_tags()}
 
     # test not passing due to page being blocked
     # test_cases = [
