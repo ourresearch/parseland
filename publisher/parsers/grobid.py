36d9d75ed3952d3476bc7fff0acf37d8cac6b941
nolan <nolanm1995@live.com>
2023-07-02 23:55:43 -0500

    Add GROBID 503 retry

--- publisher/parsers/grobid.py
+++ publisher/parsers/grobid.py
@@ -6,6 +6,7 @@ import os
 from urllib.parse import urljoin
 
 from grobid_client.models import ProcessForm
+from tenacity import retry, stop_after_attempt, wait_fixed
 
 from publisher.parsers.parser import Parser
 from grobid_client.types import File
@@ -13,10 +14,10 @@ from grobid_client.api.pdf import process_fulltext_document
 
 
 class GrobidParser(Parser):
-
     parser_name = "grobid"
 
     BASE_URL = os.getenv('GROBID_BASE_URL')
+
     # BASE_URL = 'https://kermitt2-grobid.hf.space/'
 
     def __init__(self, pdf_contents):
@@ -30,22 +31,35 @@ class GrobidParser(Parser):
         return {"authors": [], "abstract": None, "published_date": None,
                 "genre": None, 'references': []}
 
-    def parse(self):
+    @retry(stop=stop_after_attempt(5), wait=wait_fixed(3))
+    def get_grobid_soup(self):
         form = ProcessForm(
             segment_sentences="1",
             include_raw_citations="1",
             include_raw_affiliations="1",
-            input_=File(file_name="contents.pdf", payload=BytesIO(self.pdf_contents),
+            input_=File(file_name="contents.pdf",
+                        payload=BytesIO(self.pdf_contents),
                         mime_type="application/pdf"),
-                        )
+        )
         r = process_fulltext_document.sync_detailed(client=self.client,
                                                     multipart_data=form)
         soup = BeautifulSoup(r.content, parser='lxml', features='lxml')
+
+        if body_tag := soup.select_one('body'):
+            if 'HTTP ERROR 503' in body_tag.text:
+                raise Exception('503 error from GROBID')
+
+    def parse(self):
+        soup = self.get_grobid_soup()
+        body = None
+        if body_tag := soup.select_one('body'):
+            body = body_tag.text
         authors = []
         author_tags = soup.select('sourceDesc author')
         universal_affs = []
         for tag in author_tags:
-            author = {'name': None, 'affiliations': [], 'is_corresponding': tag.get('role', '') == 'corresp'}
+            author = {'name': None, 'affiliations': [],
+                      'is_corresponding': tag.get('role', '') == 'corresp'}
             if first_name := tag.select_one('forename[type=first]'):
                 last_name = tag.select_one('surname')
                 author['name'] = f'{first_name.text} {last_name.text}'
@@ -55,7 +69,8 @@ class GrobidParser(Parser):
                         label.decompose()
                     author['affiliations'].append(aff_tag.text.strip())
                 authors.append(author)
-            elif universal_aff_tag := tag.select_one('note[type=raw_affiliation]'):
+            elif universal_aff_tag := tag.select_one(
+                    'note[type=raw_affiliation]'):
                 if label := universal_aff_tag.find('label'):
                     label.decompose()
                 universal_affs.append(universal_aff_tag.text)
@@ -65,11 +80,9 @@ class GrobidParser(Parser):
         abstract = None
         if abstract_tag := soup.select_one('abstract'):
             abstract = abstract_tag.text
-        body = None
-        if body_tag := soup.select_one('body'):
-            body = body_tag.text
 
-        ref_tags = soup.select('div[type=references] listbibl biblstruct note[type=raw_reference]')
+        ref_tags = soup.select(
+            'div[type=references] listbibl biblstruct note[type=raw_reference]')
         refs = [tag.text for tag in ref_tags]
         return {'authors': authors,
                 'abstract': abstract,
